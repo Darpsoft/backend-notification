bdc008594c8859de57c8a08c0e12364b9503e33b
Pedro Fuentes <pedro@vantageio.com>
2022-08-08 13:16:05 -0400

    refactor: remove packages

--- src/controllers/messaging.controller.ts
+++ src/controllers/messaging.controller.ts
@@ -1,54 +1,34 @@
 import {Request, Response} from 'express';
 import {getCertFirebase} from '../functions/messaging.functions';
+import admin from 'firebase-admin';
 
 // This is for Firebase
-const admin = require('firebase-admin');
+
+admin.initializeApp({
+  credential: admin.credential.cert(
+    getCertFirebase(process.env.NODE_ENV ?? 'production'),
+  ),
+});
 
 const options = {
   // Required for background/quit data-only messages on iOS
   contentAvailable: true,
   // Required for background/quit data-only messages on Android
   priority: 'high',
+  // Time-to-live in seconds
+  timeToLive: 0,
 };
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export async function notification(req: Request, res: Response) {
-  /*  
-    #swagger.tags = ['Messaging']
-    #swagger.description = 'Send notification by topic' 
-
-
-    #swagger.parameters['body'] = {
-      in: 'body',
-      description: 'Body Notification', 
-      required: true,
-      schema: { $ref: "#/definitions/messaging-sendToTopic-req" }
-    }
-
-    #swagger.responses[200] = { 
-      schema: { "$ref": "#/definitions/messaging-sendToTopic-res" },
-      description: "Success send to notification"
-    }
-  */
   try {
-    admin.initializeApp({
-      credential: admin.credential.cert(
-        getCertFirebase(process.env.NODE_ENV ?? 'production'),
-      ),
-    });
-
-    const messaging = req.body;
-
-    const notificationData = {
-      data: messaging.data,
-    };
-
-    const response = await admin
-      .messaging()
-      .sendToTopic(messaging.topic, notificationData, {
-        ...options,
-        timeToLive: 3600,
-      });
+    const response = await admin.messaging().sendToDevice(
+      req.body.tokens,
+      {
+        data: req.body.data,
+      },
+      options,
+    );
     return res.status(200).json(response);
   } catch (error) {
     return res.status(404).json({error});
